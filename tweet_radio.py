85607826eefc7294112eccf15bf26b760cc0810d
Corey Lynch <coreylynch9@gmail.com>
2012-10-30 20:54:43 -0400

    first commit

---
+++ tweet_radio.py
@@ -0,0 +1,17 @@
+import tweetstream
+import re 
+import time
+import commands
+import sys
+
+punctre = re.compile('[!"#$%&\'()*+,-/:;<=>?@[\\]^_`{|}~]')
+urlre = re.compile('(?P<url>https?://[^\s]+)')
+
+with tweetstream.SampleStream('jerilynch','gracie') as stream:
+	for tweet in stream:
+		if 'text' in tweet:
+			tweet_stripped = urlre.sub('',tweet['text'])
+			tweet_stripped = punctre.sub('',tweet_stripped)
+			if sys.argv[1] in tweet['text']:
+				commands.getstatusoutput('say %s' % tweet_stripped) 
+				time.sleep(1)
